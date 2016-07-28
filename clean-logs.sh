824dc75589b4933a214d0966f55fe26e414b18a4
Phil Tsarik <phil.tsarik@gmail.com>
2016-07-28 22:58:07 +0200

    Add a script to clean system log files

---
+++ clean-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Truncate some current log file
+> /var/log/messages
+> /var/log/syslog
+> /var/log/user.log
+
+# Remove archived log files
+rm /var/log/*.0
+rm /var/log/*.1
+rm /var/log/*.gz
+rm /var/log/*.old
