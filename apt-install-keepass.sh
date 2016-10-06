7e2de8a219bda38b94a07a40a3b04ba633c0adfa
Phil Tsarik <phil.tsarik@gmail.com>
2016-10-06 18:06:29 +0200

    Add script to install and setup Keepass

---
+++ apt-install-keepass.sh
@@ -0,0 +1,18 @@
+apt-get install keepass2
+
+# KeePassRPC => keefox
+# http://askubuntu.com/questions/291002/how-to-integrate-keypass2-and-firefox-using-keefox-passifox-in-ubuntu-14-04-16/291309#291309
+# apt-get install mono-dmcs libmono-system-management4.0-cil
+
+# KeePassHttp => PassIFox
+# https://github.com/pfn/keepasshttp/
+# cd $HOME
+# wget https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx
+# sudo mv KeePassHttp.plgx /usr/lib/keepass2
+# apt-get install libmono-system-xml-linq4.0-cil \
+#                 libmono-system-data-datasetextensions4.0-cil \
+#                 libmono-system-runtime-serialization4.0-cil \
+#                 mono-mcs
+
+# Enable auto-type
+# sudo apt-get install xdotool
